25b1c5cd717e3492794e6e0ec3f8613b18ed96ed
gdyanry <569161256@qq.com>
2017-03-02 16:06:03 +0800

    完成EditText并修复了路径规划的一些bug

--- src/lib/common/util/ConsoleUtil.java
+++ src/lib/common/util/ConsoleUtil.java
@@ -11,25 +11,28 @@ import java.util.Scanner;
  *         2015年1月16日 下午2:48:32
  */
 public class ConsoleUtil {
-	public static void debug(String msg) {
-		debug(ConsoleUtil.class, msg);
+	public static void debug(String msg, Object... args) {
+		debug(ConsoleUtil.class, msg, args);
 	}
 
-	public static void error(String msg) {
-		error(ConsoleUtil.class, msg);
+	public static void error(String msg, Object... args) {
+		error(ConsoleUtil.class, msg, args);
 	}
 
-	public static void debug(Class<?> loggerClass, String msg) {
-		System.out.println(getLog(loggerClass, msg));
+	public static void debug(Class<?> loggerClass, String msg, Object... args) {
+		System.out.println(getLog(loggerClass, msg, args));
 	}
 
-	public static void error(Class<?> loggerClass, String msg) {
-		System.err.println(getLog(loggerClass, msg));
+	public static void error(Class<?> loggerClass, String msg, Object... args) {
+		System.err.println(getLog(loggerClass, msg, args));
 	}
 
-	public static String getLog(Class<?> loggerClass, String msg) {
+	public static String getLog(Class<?> loggerClass, String msg, Object... args) {
 		if (loggerClass == null) {
 			loggerClass = ConsoleUtil.class;
+		}
+		if (args.length > 0) {
+			msg = String.format(msg, args);
 		}
 		boolean isLogger = false;
 		StackTraceElement[] frames = Thread.currentThread().getStackTrace();
