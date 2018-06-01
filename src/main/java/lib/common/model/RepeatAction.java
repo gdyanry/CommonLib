46055421c7526209e6cb0fdc2767ad7fbb6c11b7
gdyanry <ryyanc@isoftstone.com>
2018-06-01 09:55:46 +0800

    add comments to RepeatAction

--- src/main/java/lib/common/model/RepeatAction.java
+++ src/main/java/lib/common/model/RepeatAction.java
@@ -1,16 +1,21 @@
 package lib.common.model;
 
 /**
+ * 该类代表某个连续重复的动作，例如连接点击某个按钮。
  * @author rongyu.yan
  * @date 2018/3/22.
  */
 
-public abstract class ContinuousAction {
+public abstract class RepeatAction {
     private long maxInterval;
     private int count;
     private long lastTimestamp;
 
-    public ContinuousAction(long maxInterval) {
+    /**
+     *
+     * @param maxInterval 重复动作之间的最大时间间隔。如果先后两个动作的间隔超出此时间，则计数重置为0。
+     */
+    public RepeatAction(long maxInterval) {
         this.maxInterval = maxInterval;
     }
 
@@ -23,5 +28,9 @@ public abstract class ContinuousAction {
         consumeAction(++count);
     }
 
+    /**
+     *
+     * @param count 动作触发的次数。
+     */
     protected abstract void consumeAction(int count);
 }
