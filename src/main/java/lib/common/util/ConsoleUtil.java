21a2ae91feb85f548446013c77bb124c5df39186
ywx577674 <yanrongyu2@huawei.com>
2018-06-29 09:54:32 +0800

    日志显示的行号修改为可调整

--- src/main/java/lib/common/util/ConsoleUtil.java
+++ src/main/java/lib/common/util/ConsoleUtil.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package lib.common.util;
 
@@ -7,46 +7,39 @@ import java.util.Scanner;
 
 /**
  * @author yanry
- *
- *         2015年1月16日 下午2:48:32
+ * <p>
+ * 2015年1月16日 下午2:48:32
  */
 public class ConsoleUtil {
-	public static void debug(String msg, Object... args) {
-		debug(ConsoleUtil.class, msg, args);
-	}
+    public static void debug(String msg, Object... args) {
+        debug(0, msg, args);
+    }
 
-	public static void error(String msg, Object... args) {
-		error(ConsoleUtil.class, msg, args);
-	}
+    public static void error(String msg, Object... args) {
+        error(0, msg, args);
+    }
 
-	public static void debug(Class<?> loggerClass, String msg, Object... args) {
-		System.out.println(getLog(loggerClass, msg, args));
-	}
+    public static void debug(int depthOffset, String msg, Object... args) {
+        System.out.println(getLog(depthOffset, msg, args));
+    }
 
-	public static void error(Class<?> loggerClass, String msg, Object... args) {
-		System.err.println(getLog(loggerClass, msg, args));
-	}
+    public static void error(int depthOffset, String msg, Object... args) {
+        System.err.println(getLog(depthOffset, msg, args));
+    }
 
-	public static String getLog(Class<?> loggerClass, String msg, Object... args) {
-		if (loggerClass == null) {
-			loggerClass = ConsoleUtil.class;
-		}
-		if (args.length > 0) {
-			msg = String.format(msg, args);
-		}
-		boolean isLogger = false;
-		StackTraceElement[] frames = Thread.currentThread().getStackTrace();
-		for (StackTraceElement f : frames) {
-			if (f.getClassName().equals(loggerClass.getName())) {
-				isLogger = true;
-			} else if (isLogger) {
-				String fileName = f.getFileName();
-				fileName = fileName.substring(0, fileName.lastIndexOf("."));
-				return String.format("[%s:%s.%s] %s", Thread.currentThread().getName(), fileName, f.getLineNumber(), msg);
-			}
-		}
-		return String.format("[%s] %s", Thread.currentThread().getName(), msg);
-	}
+    public static String getLog(int depthOffset, String msg, Object... args) {
+        if (args.length > 0) {
+            msg = String.format(msg, args);
+        }
+        StackTraceElement[] frames = Thread.currentThread().getStackTrace();
+        if (frames.length > depthOffset + 1) {
+            StackTraceElement f = frames[1 + depthOffset];
+            String fileName = f.getFileName();
+            fileName = fileName.substring(0, fileName.lastIndexOf("."));
+            return String.format("[%s:%s.%s] %s", Thread.currentThread().getName(), fileName, f.getLineNumber(), msg);
+        }
+        return String.format("[%s] %s", Thread.currentThread().getName(), msg);
+    }
 
     private static Scanner scanner = new Scanner(System.in);
 
