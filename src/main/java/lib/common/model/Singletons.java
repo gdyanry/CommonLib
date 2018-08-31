983ebf3b04455e16c8321513956b71180d71496b
rongyu.yan <rongyu.yan@tcl.com>
2018-08-31 11:02:25 +0800

    改用软引用持有单例对象

--- src/main/java/lib/common/model/Singletons.java
+++ src/main/java/lib/common/model/Singletons.java
@@ -1,58 +1,67 @@
 /**
- * 
+ *
  */
 package lib.common.model;
 
+import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author yanry
- *
- *         2016年5月22日
+ * <p>
+ * 2016年5月22日
  */
-@SuppressWarnings("unchecked")
 public final class Singletons {
 
-	private static Map<Class<?>, Object> container = new HashMap<Class<?>, Object>();
+    private static Map<Class<?>, SoftReference<Object>> container = new HashMap<>();
+    private static ObjectProvider objectProvider;
 
-	private Singletons() {
-	}
+    private Singletons() {
+    }
 
-	/**
-	 * 
-	 * @param type
-	 *            this class must have an accessible parameterless constructor.
-	 * @return
-	 */
-	public static <T> T get(Class<T> type) {
-		T obj = (T) container.get(type);
-		if (obj == null) {
-			synchronized (type) {
-				obj = (T) container.get(type);
-				if (obj == null) {
-					try {
-						obj = type.newInstance();
-						container.put(type, obj);
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				}
-			}
-		}
-		return obj;
-	}
+    public static void setObjectProvider(ObjectProvider objectProvider) {
+        Singletons.objectProvider = objectProvider;
+    }
 
-	/**
-	 * 
-	 * @param type
-	 * @return the previous created object of the given type, might be null.
-	 */
-	public static <T> T remove(Class<T> type) {
-		return (T) container.remove(type);
-	}
-	
-	public static void release() {
-		container.clear();
-	}
+    public static <T> T get(Class<T> type) {
+        SoftReference<Object> reference = container.get(type);
+        if (reference == null || reference.get() == null) {
+            synchronized (type) {
+                if (reference == null || reference.get() == null) {
+                    Object obj = null;
+                    if (objectProvider != null) {
+                        obj = objectProvider.getInstance(type);
+                    }
+                    if (obj == null) {
+                        try {
+                            obj = type.getDeclaredConstructor().newInstance();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    reference = new SoftReference<>(obj);
+                    container.put(type, reference);
+                }
+            }
+        }
+        return (T) reference.get();
+    }
+
+    /**
+     * @param type
+     * @return the previous created object of the given type, might be null.
+     */
+    public static <T> T remove(Class<T> type) {
+        SoftReference<Object> reference = container.remove(type);
+        return reference == null ? null : (T) reference.get();
+    }
+
+    public static void release() {
+        container.clear();
+    }
+
+    public interface ObjectProvider {
+        <T> T getInstance(Class<T> type);
+    }
 }
