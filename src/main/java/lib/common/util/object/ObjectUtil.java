9f26ee8d5ee57f0fa5708a2c1f4eb4355405a6a6
rongyu.yan <rongyu.yan@tcl.com>
2018-09-19 18:39:12 +0800

    添加@Presentable

--- src/main/java/lib/common/util/object/ObjectUtil.java
+++ src/main/java/lib/common/util/object/ObjectUtil.java
@@ -1,8 +1,14 @@
 package lib.common.util.object;
 
+import lib.common.model.json.JSONArray;
+import lib.common.model.json.JSONObject;
 import lib.common.model.log.Logger;
+import lib.common.util.StringUtil;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -41,6 +47,62 @@ public class ObjectUtil {
             }
         }
         return true;
+    }
+
+    /**
+     *
+     * @param object
+     * @param typeSymbol
+     * @return the return object is one of these types: Boolean, Double, Integer, JSONArray, JSONObject, Long, String, or the JSONObject.NULL object.
+     */
+    public static Object getPresentation(Object object, String typeSymbol) {
+        if (object == null) {
+            return null;
+        }
+        Class<?> type = object.getClass();
+        if (type.isAnnotationPresent(Presentable.class)) {
+            JSONObject jsonObject = new JSONObject().put(typeSymbol, StringUtil.getClassName(object));
+            for (Method method : type.getMethods()) {
+                if (method.isAnnotationPresent(Presentable.class)) {
+                    String key = StringUtil.setFirstLetterCase(method.getName().replaceFirst("^get", ""), false);
+                    try {
+                        Object value = method.invoke(object);
+                        if (value != null) {
+                            jsonObject.put(key, getPresentation(value, typeSymbol));
+                        }
+                    } catch (ReflectiveOperationException e) {
+                        Logger.getDefault().catches(e);
+                    }
+                }
+            }
+            return jsonObject;
+        }
+        if (object instanceof Enum) {
+            return ((Enum) object).name();
+        }
+        if (object.getClass().isArray()) {
+            JSONArray jsonArray = new JSONArray();
+            int len = Array.getLength(object);
+            for (int i = 0; i < len; i++) {
+                jsonArray.put(getPresentation(Array.get(object, i), typeSymbol));
+            }
+            return jsonArray;
+        } else if (object instanceof List) {
+            JSONArray jsonArray = new JSONArray();
+            List list = (List) object;
+            for (Object item : list) {
+                jsonArray.put(getPresentation(item, typeSymbol));
+            }
+            return jsonArray;
+        } else if (object instanceof Map) {
+            JSONArray jsonArray = new JSONArray();
+            Map map = (Map) object;
+            for (Object key : map.keySet()) {
+                jsonArray.put(new JSONArray().put(getPresentation(key, typeSymbol)).put(getPresentation(map.get(key), typeSymbol)));
+            }
+            return jsonArray;
+        }
+        return object;
     }
 }
 
