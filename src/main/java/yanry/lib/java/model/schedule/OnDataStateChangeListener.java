a32489d984360e06f227aba1bec131f6d02cbe45
rongyu.yan <rongyu.yan@tcl.com>
2019-12-15 18:38:43 +0800

    #

--- src/main/java/yanry/lib/java/model/schedule/OnDataStateChangeListener.java
+++ src/main/java/yanry/lib/java/model/schedule/OnDataStateChangeListener.java
@@ -5,6 +5,8 @@ package yanry.lib.java.model.schedule;
  */
 public interface OnDataStateChangeListener {
     /**
+     * ShowData状态变化的回调，此时调用{@link ShowData#getState()}拿到的是旧的状态。
+     *
      * @param toState {@link ShowData#STATE_ENQUEUE} means exactly after enqueue;
      *                {@link ShowData#STATE_DEQUEUE} means exactly before dequeue.
      *                {@link ShowData#STATE_SHOWING} means exactly after shown;
