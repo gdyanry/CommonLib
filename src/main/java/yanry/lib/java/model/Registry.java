ee2a5371ce9c623329b6d8dca3e7f73fa8285d2e
rongyu.yan <rongyu.yan@tcl.com>
2020-06-04 18:34:39 +0800

    Registry改为线程安全

--- src/main/java/yanry/lib/java/model/Registry.java
+++ src/main/java/yanry/lib/java/model/Registry.java
@@ -2,25 +2,26 @@ package yanry.lib.java.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by yanry on 2020/5/8.
  */
 public class Registry<T> {
-    private LinkedList<T> registrants;
-
-    public Registry() {
-        registrants = new LinkedList<>();
-    }
+    private ConcurrentLinkedQueue<T> registrants;
 
     public boolean register(T registrant) {
         if (registrants == null) {
-            registrants = new LinkedList<>();
-            registrants.add(registrant);
-            return true;
-        } else if (registrants.contains(registrant)) {
+            synchronized (this) {
+                if (registrants == null) {
+                    registrants = new ConcurrentLinkedQueue<>();
+                    registrants.add(registrant);
+                    return true;
+                }
+            }
+        }
+        if (registrants.contains(registrant)) {
             return false;
         }
         return registrants.add(registrant);
